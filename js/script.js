c432f4de853e70e80d2fde3b05a7d75956d01185
Dmitrii <dbjenov@gmail.com>
2021-07-01 11:24:16 +0300

    Fixed menu burger and pagination + fixed responsive of buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
+//Burger js
+
 $(document).ready(function() {
     $('.header-menu__burger').click(function(e) {
         $('.header-menu__burger,.header-menu__link,.header').toggleClass('active')
     })
+    $('#HP').click(function(e){
+        $('.header-menu__burger,.header-menu__link,.header').removeClass('active')
+    })
+    $('#HP1').click(function(e){
+        $('.header-menu__burger,.header-menu__link,.header').removeClass('active')
+    })
+    $('#HP2').click(function(e){
+        $('.header-menu__burger,.header-menu__link,.header').removeClass('active')
+    })
+    $('#HP3').click(function(e){
+        $('.header-menu__burger,.header-menu__link,.header').removeClass('active')
+    })
 })
+
