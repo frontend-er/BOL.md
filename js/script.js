75f4a6c29d016b9bf54431e10e907a5ceaef02c2
Dmitrii <dbjenov@gmail.com>
2021-07-02 12:00:53 +0300

    footer starrted + adaptive

--- js/script.js
+++ js/script.js
@@ -16,5 +16,27 @@ $(document).ready(function() {
     $('#HP3').click(function(e){
         $('.header-menu__burger,.header-menu__link,.header').removeClass('active')
     })
+
+    //Slick slider
+
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+
 })
+
 
