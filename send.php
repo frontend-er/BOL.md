6fae3c692675296294339a3c5c8c4fd775c6c748
Dmitrii <dbjenov@gmail.com>
2021-07-10 13:17:43 +0300

    form to email

--- send.php
+++ send.php
@@ -1,16 +1,25 @@
 <?php
 $fio = $_POST['name'];
 $email = $_POST['email'];
+$phone = $_POST['phone'];
+$question = $_POST['question'];
+
+
+$question = htmlspecialchars($question);
 $fio = htmlspecialchars($fio);
+
 $email = htmlspecialchars($email);
+$phone = htmlspecialchars($phone);
 $fio = urldecode($fio);
 $email = urldecode($email);
+$phone = urldecode($phone);
+$question = urldecode($question);
+
+$question = trim($question);
+$phone = trim($phone);
 $fio = trim($fio);
 $email = trim($email);
-echo $fio;
-echo "<br>";
-echo $email;
-if (mail("dbjenov@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: bajenov.dima.oleg@gmail.com \r\n"))
+if (mail("dbjenov@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email , "Phone: " .$phone, "Question: ".$question, "From: bajenov.dima.oleg@gmail.com \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
