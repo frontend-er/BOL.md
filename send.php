3aba5d332dabcaf8d6f13c59d72e6be2abe94bae
Dmitrii <dbjenov@gmail.com>
2021-07-19 13:01:24 +0300

    reCapcha1

--- send.php
+++ send.php
@@ -1,22 +1,6 @@
 <?php
-
-
-
-
-
-// ваш секретный ключ
-$secret = '6LdHH6cbAAAAAA4yNOY3t-iCoEtyQjxpKCnSnyXj';
-// однократное включение файла autoload.php (клиентская библиотека reCAPTCHA PHP)
-require_once (dirname(__FILE__).'/recaptcha/autoload.php');
-// если в массиве $_POST существует ключ g-recaptcha-response, то...
-if (isset($_POST['g-recaptcha-response'])) {
-  // создать экземпляр службы recaptcha, используя секретный ключ
-  $recaptcha = new \ReCaptcha\ReCaptcha($secret);
-  // получить результат проверки кода recaptcha
-  $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-  // если результат положительный, то...
-  $fio = $_POST['name'];
-    $email = $_POST['email'];
+$fio = $_POST['name'];
+$email = $_POST['email'];
 $phone = $_POST['phone'];
 $question = $_POST['question'];
 
@@ -36,15 +20,11 @@ $phone = trim($phone);
 $fio = trim($fio);
 $email = trim($email);
 if (mail("dbjenov@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email. ". Phone: " .$phone.". Question: ".$question,  "From: bajenov.dima.oleg@gmail.com \r\n"))
- {    
-     echo "<h1> Form was succes send </h1>";
+ {      echo "<h1> Form was succes send </h1>";
+} else {
+    echo "Error wit form sending";
 }
 
-  } else {
-    // иначе передать ошибку
-    $errors = $resp->getErrorCodes();
-    $data['error-captcha']=$errors;
-    $data['msg']='Код капчи не прошёл проверку на сервере';
-    $data['result']='error';
-  }
+
+
 
