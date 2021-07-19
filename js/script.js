eff0d832f96a538e337039115d771fe2786f54bc
Dmitrii <dbjenov@gmail.com>
2021-07-19 12:50:26 +0300

    reCapcha

--- js/script.js
+++ js/script.js
@@ -105,4 +105,42 @@ $(document).ready(function () {
 
     })
 
+
+
+
+
+
+    // reCapcha
+
+    // Работа с виджетом recaptcha
+    // 1. Получить ответ гугл капчи
+    var captcha = grecaptcha.getResponse();
+
+    // 2. Если ответ пустой, то выводим сообщение о том, что пользователь не прошёл тест.
+    // Такую форму не будем отправлять на сервер.
+    if (!captcha.length) {
+        // Выводим сообщение об ошибке
+        $('#recaptchaError').text('* Вы не прошли проверку "Я не робот"');
+    } else {
+        // получаем элемент, содержащий капчу
+        $('#recaptchaError').text('');
+    }
+
+    // 3. Если форма валидна и длина капчи не равно пустой строке, то отправляем форму на сервер (AJAX)
+    if ((formValid) && (captcha.length)) {
+
+        // добавить в formData значение 'g-recaptcha-response'=значение_recaptcha
+        formData.append('g-recaptcha-response', captcha);
+
+    }
+
+    // 4. Если сервер вернул ответ error, то делаем следующее...
+    // Сбрасываем виджет reCaptcha
+    grecaptcha.reset();
+    // Если существует свойство msg у объекта $data, то...
+    if ($data.msg) {
+        // вывести её в элемент у которого id=recaptchaError
+        $('#recaptchaError').text($data.msg);
+    }
+
 })
--- send.php
+++ send.php
@@ -23,4 +23,35 @@ if (mail("dbjenov@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-
  {      echo "<h1> Form was succes send </h1>";
 } else {
     echo "Error wit form sending";
-}?>
+}
+
+
+
+
+
+// ваш секретный ключ
+$secret = '6LdHH6cbAAAAAA4yNOY3t-iCoEtyQjxpKCnSnyXj';
+// однократное включение файла autoload.php (клиентская библиотека reCAPTCHA PHP)
+require_once (dirname(__FILE__).'/recaptcha/autoload.php');
+// если в массиве $_POST существует ключ g-recaptcha-response, то...
+if (isset($_POST['g-recaptcha-response'])) {
+  // создать экземпляр службы recaptcha, используя секретный ключ
+  $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+  // получить результат проверки кода recaptcha
+  $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+  // если результат положительный, то...
+  if ($resp->isSuccess()){
+    // действия, если код captcha прошёл проверку
+    //...
+  } else {
+    // иначе передать ошибку
+    $errors = $resp->getErrorCodes();
+    $data['error-captcha']=$errors;
+    $data['msg']='Код капчи не прошёл проверку на сервере';
+    $data['result']='error';
+  }
+
+} else {
+  //ошибка, не существует ассоциативный массив $_POST["send-message"]
+  $data['result']='error';
+}
